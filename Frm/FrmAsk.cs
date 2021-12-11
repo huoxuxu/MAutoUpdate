5198891cb39a8610d6d2a989e99c4f665362da94
huoxuxu <h_xuxu@sina.com>
2021-12-11 22:10:19 +0800

    add askform

---
+++ Frm/FrmAsk.cs
@@ -0,0 +1,108 @@
+﻿using Ionic.Zip;
+using MAutoUpdate.Models;
+using MAutoUpdate.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace MAutoUpdate
+{
+    public partial class FrmAsk : Form
+    {
+        private UpdateWorkService work;
+
+        public FrmAsk(UpdateWorkService work)
+        {
+            InitializeComponent();
+
+            this.work = work;
+            this.lblContent.Text = work.context.UpgradeInfo.UpgradeContent;
+        }
+
+        #region 让窗体变成可移动
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+
+        [DllImport("User32.dll")]
+        private static extern IntPtr WindowFromPoint(Point p);
+
+        public const int WM_SYSCOMMAND = 0x0112;
+        public const int SC_MOVE = 0xF010;
+        public const int HTCAPTION = 0x0002;
+        private IntPtr moveObject = IntPtr.Zero;    //拖动窗体的句柄
+
+        private void PNTop_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (moveObject == IntPtr.Zero)
+            {
+                if (this.Parent != null)
+                {
+                    moveObject = this.Parent.Handle;
+                }
+                else
+                {
+                    moveObject = this.Handle;
+                }
+            }
+            ReleaseCapture();
+            SendMessage(moveObject, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 稍后更新,则将更新程序关闭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnUpdateLater_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// 开始升级
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnUpdateNow_Click(object sender, EventArgs e)
+        {
+            this.Hide();// 隐藏当前窗口
+
+            UpdateForm updateForm = new UpdateForm(this.work);
+            if (updateForm.ShowDialog() == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        /// <summary>
+        /// 忽略
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnIgnore_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        #region 辅助
+        #endregion
+
+
+    }
+}
